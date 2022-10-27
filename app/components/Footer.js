d3c225434d6b4fd90131fea19388c384fd31182d
benji920 <leskids92@gmail.com>
2022-10-27 11:46:34 +0200

    feat: pages

---
+++ app/components/Footer.js
@@ -0,0 +1,10 @@
+
+export default function Footer(){
+  return (
+    <footer className="bg-slate-200 px-10 py-2 text-center [&_svg]:inline-block">
+      <p>
+        Powered by Mineur Création du numérique
+      </p>
+    </footer>
+  )
+}
